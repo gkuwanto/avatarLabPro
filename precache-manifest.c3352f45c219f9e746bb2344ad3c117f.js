611d8b9026f39a1ab8abe35d72b0d6cb4ab3b3d2
Garry Kuwanto <gkuwanto@gmail.com>
2020-06-10 23:52:07 +0700

    Updates

---
+++ precache-manifest.c3352f45c219f9e746bb2344ad3c117f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c9324d489cbfd90a9dd9b1046c948d",
+    "url": "/avatarLabPro/index.html"
+  },
+  {
+    "revision": "c8a70622f974e1fab495",
+    "url": "/avatarLabPro/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ec102557f4deda4f4552",
+    "url": "/avatarLabPro/static/js/2.dad8d524.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/avatarLabPro/static/js/2.dad8d524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8a70622f974e1fab495",
+    "url": "/avatarLabPro/static/js/main.9b5ceb6c.chunk.js"
+  },
+  {
+    "revision": "4e15f27005dee841a0dd",
+    "url": "/avatarLabPro/static/js/runtime-main.f64bc9da.js"
+  }
+]);
